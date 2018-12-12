1ea80c1d3d3a17061757ea7de07520e710c7032f
zaheer <m.abbaszaheer@gmail.com>
2018-12-12 00:53:40 -0500

    solution of TODO for Ruby basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,44 @@
+#require 'pry', Please do not mind line 1, 41, and 44, I uses it to debug Pizza_party
+#division given two numbers returns quotient of the first number divided by the second number
 def division(num1, num2)
- 
+	quotient=num1/num2
 end
 
+
+#takes an argument of a persons name and assign it to a variable name
 def assign_variable(value)
-  
+    value=value
 end
 
-def argue
-  
+
+#argue accepts an argument and return that argument as is
+def argue (argument)
+    argument
 end
 
-def greeting
+
+#greeting takes two arguments, I suppose , This function may not need additional functionality since it passed the test
+def greeting (greeting, name)
 
 end
 
+
+#returns the phrase "Nice"
 def return_a_value
-
+     phrase="Nice"
 end
 
+
+#last_evaluated_value returns the phrase "expert"
 def last_evaluated_value
-
+    phrase="expert"
 end
 
-def pizza_party
 
+#pizza_party 1) returns "cheese" by default#pizza_party 2) returns the argument it received
+def pizza_party (argument="cheese")
+argument
+#binding.pry
 end
+
+#pizza_party ("cheese")
